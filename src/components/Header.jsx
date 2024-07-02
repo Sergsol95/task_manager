b206ea19ebc6433d210ccd6e581d1c9742df4c9e
Sergsol95e <print.string@yandex.ru>
2024-07-02 20:11:58 +0300

    init continue

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <header>
+        <input type="search" placeholder="Search everything" />
+        <div className="notifications">🔔</div>
+        <div className="user-profile">
+            <img src="" alt="User Avatar" />
+            <span></span>
+        </div>
+    </header>
+);
+
+export default Header;
