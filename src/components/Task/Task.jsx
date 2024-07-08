fa09f9254efd851acfd569191580b3087ddc8655
Sergsol95e <print.string@yandex.ru>
2024-07-08 20:06:41 +0300

    delete added

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -7,13 +7,31 @@ const TaskItemContainer = styled.div`
   margin: 10px 0;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   border-radius: 10px; 
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const Task = ({ title, description }) => {
+const DeleteButton = styled.button`
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const TaskDivider = styled.div`
+`
+
+const Task = ({ title, description, onDelete }) => {
   return (
     <TaskItemContainer>
+      <TaskDivider>
       <h3>{title}</h3>
       <p>{description}</p>
+      </TaskDivider>
+      <DeleteButton onСlick={onDelete}>Delete</DeleteButton>
     </TaskItemContainer>
   );
 };
--- src/theme.jsx
+++ src/theme.jsx
@@ -4,6 +4,9 @@ const theme = {
       secondary: '#e0e0e0',
       background: '#fff',
       text: '#333',
+      primaryDark: '#0056b3',
+      danger: '#dc3545',
+      dangerDark: '#c82333',
     },
     sizes: {
       font: '16px',
